2f90728b3c23486bf49011b91358670f7a4ec0d2
Vsevolod Pletnev <Vsevolod.Pletnev@farcana.com>
2024-08-30 12:58:40 +0700

    fixes

--- source/client/main.ts
+++ source/client/main.ts
@@ -1,27 +1,61 @@
-const ws = new WebSocket("ws://localhost:8000");
+const connect = () => {
+  const ws = new WebSocket("wss://portal-vqhj.onrender.com");
 
-ws.onopen = function (e) {
-  console.log("[open] Connection established");
-  console.log("Sending to server");
-  ws.send("ping");
+  ws.onopen = function (e) {
+    console.log("[open] Connection established");
+    console.log("Sending to server");
+    ws.send("ping");
+  };
+
+  ws.onmessage = function (event) {
+    try {
+      // console.log(`[message] Data received from server: ${event.data}`);
+      const msg = JSON.parse(event.data);
+      ws.send(JSON.stringify({
+        id: msg.id,
+        body: "Test",
+      }));
+    } catch (e) {}
+  };
+
+  ws.onclose = function (event) {
+    if (event.wasClean) {
+      // console.log(
+      //   `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`,
+      // );
+    } else {
+      // e.g. server process killed or network down
+      // event.code is usually 1006 in this case
+      console.log("[close] Connection died");
+    }
+
+    setTimeout(function () {
+      connect();
+    }, 1000);
+  };
+
+  ws.onerror = function (error) {
+    console.log(`[error]`);
+  };
 };
 
-ws.onmessage = function (event) {
-  console.log(`[message] Data received from server: ${event.data}`);
-};
+connect();
 
-ws.onclose = function (event) {
-  if (event.wasClean) {
-    console.log(
-      `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`,
-    );
-  } else {
-    // e.g. server process killed or network down
-    // event.code is usually 1006 in this case
-    console.log("[close] Connection died");
-  }
-};
+// Tester
+setInterval(async () => {
+  try {
+    await fetch("http://localhost:8000/hi", {
+      method: "POST",
+      body: JSON.stringify({
+        "data": Math.random(),
+      }),
+    });
+  } catch (e) {}
+}, 1000);
 
-ws.onerror = function (error) {
-  console.log(`[error]`);
-};
+Deno.serve({
+  port: 1000,
+}, async (req: Request) => {
+  console.log(req);
+  return new Response("ok");
+});
--- source/server/main.ts
+++ source/server/main.ts
@@ -1,8 +1,45 @@
-function handler(req: Request): Response {
+import { ulid } from "jsr:@std/ulid";
+import { delay } from "https://deno.land/x/delay@v0.2.0/mod.ts";
+
+let clientSocket: WebSocket | null = null;
+const incomingMessageBuffer: any = {};
+
+async function handler(req: Request): Promise<Response> {
   const upgrade = req.headers.get("upgrade");
 
   if (upgrade == null) {
-    return new Response("Hello, World!");
+    const id = ulid();
+    const url = req.url;
+    const headers = req.headers;
+    const body = await req.bytes();
+
+    if (clientSocket) {
+      console.log(`req  ${id}`);
+      clientSocket.send(JSON.stringify({
+        id,
+        url,
+        headers,
+        body,
+      }));
+    }
+
+    let waitingTime = 2000;
+
+    // console.log(Object.keys(incomingMessageBuffer).length);
+
+    while (waitingTime) {
+      waitingTime--;
+      if (incomingMessageBuffer[id] !== undefined) {
+        delete incomingMessageBuffer[id];
+        console.log(`resp ${id}`);
+        return new Response("OK");
+      }
+
+      await delay(1);
+    }
+
+    console.log(`timeout ${id}`);
+    return new Response("timeout");
   } else {
     if (req.headers.get("upgrade") != "websocket") {
       return new Response(null, { status: 501 });
@@ -12,14 +49,22 @@ function handler(req: Request): Response {
 
     socket.addEventListener("open", () => {
       console.log("a client connected!");
+      clientSocket = socket;
     });
 
     socket.addEventListener("message", (event) => {
       if (event.data === "ping") {
         console.log("ping");
         socket.send("pong");
+      } else {
+        const msg = JSON.parse(event.data);
+        incomingMessageBuffer[msg.id] = msg;
       }
     });
+
+    socket.addEventListener("close", (event) => {});
+
+    socket.addEventListener("error", (event) => {});
 
     return response;
   }
